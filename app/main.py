a7f5da43202e2845e34b8889f885040c82773894
Виктория Голубова <v.golubova@denexy.com>
2026-01-15 13:25:05 +0600

    Created point of application`s start.

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from .routers import users
+from app.core.db import create_db_and_tables
+
+app = FastAPI()
+
+app.include_router(users.router)
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
