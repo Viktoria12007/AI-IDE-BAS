e47948c816dd5c3e168d4a517246e06595ab4027
Виктория Голубова <v.golubova@denexy.com>
2026-01-16 14:52:22 +0600

    Added CORSMiddleware.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import users
 from app.core.db import create_db_and_tables
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 
